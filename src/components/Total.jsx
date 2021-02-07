8af59beb736e39b95a6442a8a90da1e41f0287e3
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-07 18:56:29 -0500

    feat(step4): objects placed into an array

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Total = (props) => {
-  const { exercises1, exercises2, exercises3 } = props
+  const { parts } = props
 
   return (
     <>
-      <p><strong>Number of exercises {exercises1 + exercises2 + exercises3}</strong></p>
+      <p>
+        <strong>
+          Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+        </strong>
+      </p>
     </>
   )
 }
