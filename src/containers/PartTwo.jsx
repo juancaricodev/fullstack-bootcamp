6126dffb1de13a6165f935a4d1aabdc52074dd8b
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-08 22:52:10 -0500

    feat[1.6]: exercise developed, pending refactoring

--- src/containers/PartTwo.jsx
+++ src/containers/PartTwo.jsx
@@ -5,14 +5,36 @@ const PartTwo = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(() => good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(() => neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(() => bad + 1)
+  }
+
   return (
     <>
-      <h2 className='part-tag'>
-        Exercises 1.6 to 1.14 WIP
-      </h2>
+      <h2 className='part-tag'>Exercises 1.6 to 1.14 - WIP</h2>
 
-      <div className="part-two">
-        course code
+      <div className='part-two'>
+        <h1>Give Feedback</h1>
+
+        <div className='part-two__btns'>
+          <button type='button' onClick={handleGoodClick}>Good</button>
+          <button type='button' onClick={handleNeutralClick}>Neutral</button>
+          <button type='button' onClick={handleBadClick}>Bad</button>
+        </div>
+
+        <h1>Statistics</h1>
+
+        <div className='part-two__stats'>Good: {good}</div>
+        <div className='part-two__stats'>Neutral: {neutral}</div>
+        <div className='part-two__stats'>Bad: {bad}</div>
       </div>
     </>
   )
