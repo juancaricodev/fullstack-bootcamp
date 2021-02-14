537b06435bd022bcbfc4872be5c310a379c775b7
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-14 12:29:05 -0500

    feat[2.6](Phonebook): initial implementation for working with the container

--- src/containers/Phonebook.jsx
+++ src/containers/Phonebook.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Phonebook = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
   return (
-    <div>
-      The Phonebook
-    </div>
+    <>
+      <div className='part-tag'>
+        <h2>The Phonebook</h2>
+        <h3>Exercises 2.6 to 2.10</h3>
+      </div>
+
+      <div className='phonebook'>
+        <h2>Phonebook</h2>
+        <form>
+          <div>
+            Name: <input type='text' />
+          </div>
+          <div>
+            <button type='submit'>add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        ...
+      </div>
+    </>
   )
 }
 
