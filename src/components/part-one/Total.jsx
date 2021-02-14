f416c4c999d3faa3d312d53ba0795d491957b457
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-13 19:38:44 -0500

    refactor: global styles refactored; Total component of PartOne container refactored

--- src/components/part-one/Total.jsx
+++ src/components/part-one/Total.jsx
@@ -4,13 +4,9 @@ const Total = (props) => {
   const { course } = props
 
   return (
-    <>
-      <p>
-        <strong>
-          Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
-        </strong>
-      </p>
-    </>
+    <strong>
+      Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+    </strong>
   )
 }
 
