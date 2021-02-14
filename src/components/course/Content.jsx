c1c782da67cbf95e076fb6083a8eaca4dc6d9393
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-13 20:38:50 -0500

    feat[2.1]: Course container updated so that now is mapping parts from state

--- src/components/course/Content.jsx
+++ src/components/course/Content.jsx
@@ -6,11 +6,13 @@ const Content = (props) => {
   const { course } = props
 
   return (
-    <>
-      <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
-      <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
-      <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
-    </>
+    <ul>
+      {course.parts.map((item) => (
+        <li key={item.id}>
+          <Part part={item.name} exercises={item.exercises} />
+        </li>
+      ))}
+    </ul>
   )
 }
 
