e4e5c5e0fd63560813da474c7864199a6c073058
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-16 17:35:32 -0500

    feat[2.12](Countries): state for search  field and input and form handlers implemented

--- src/containers/Countries.jsx
+++ src/containers/Countries.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from 'react'
 
 const Countries = () => {
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('form submited')
+    console.log(search)
+  }
+
+  const handleInputChange = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <>
       <div className='part-tag'>
@@ -9,10 +21,16 @@ const Countries = () => {
       </div>
 
       <div className='countries'>
-        <div className='search'>
-          <h3>Find Countries</h3>
-          <input type='text' />
-        </div>
+        <h3>Find Countries</h3>
+
+        <form className='search' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={search}
+            onChange={handleInputChange}
+          />
+          <button type='submit'>Search</button>
+        </form>
       </div>
     </>
   )
