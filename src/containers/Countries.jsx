f5492b74ef3532b1620d8e5738f4dd7810aa5862
Juan Camilo Rico <juancarico92@gmail.com>
2021-02-16 21:20:25 -0500

    feat[2.12](Countries): validation and rendering for individual element in filter

--- src/containers/Countries.jsx
+++ src/containers/Countries.jsx
@@ -54,11 +54,31 @@ const Countries = () => {
               filter.length > 10
                 ? <h3>Too many matches, specify another filter</h3>
                 : (
-                  <ul>
-                    {filter.map(country => (
-                      <li key={country.name}>{country.name}</li>
-                    ))}
-                  </ul>
+                  filter.length === 1
+                    ? (
+                      <ul>
+                        {filter.map(country => (
+                          <li key={country.name}>
+                            <h1>{country.name}</h1>
+                            <p>Capital: {country.capital}</p>
+                            <p>Population: {country.population}</p>
+                            <h3>Languages</h3>
+                            <ul>
+                              {country.languages.map(language => (
+                                <li key={language.name}>{language.name}</li>
+                              ))}
+                            </ul>
+                            <img src={country.flag} alt={`${country.demonym} flag`} />
+                          </li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <ul>
+                        {filter.map(country => (
+                          <li key={country.name}>{country.name}</li>
+                        ))}
+                      </ul>
+                    )
                 )
             )
             : <h3>No match for this search</h3>
